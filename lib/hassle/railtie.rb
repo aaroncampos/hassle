925823e6a12291e1dbdbb573efdb9b5daed9c42c
Aaron Campos <frater.arc@gmail.com>
2011-06-20 07:33:50 -0700

    Use hassle when running on heroku, regardless of environment setting. This allows for environments like staging, etc.

--- lib/hassle/railtie.rb
+++ lib/hassle/railtie.rb
@@ -8,7 +8,7 @@ begin
         require 'sass'
         require 'sass/plugin'
 
-        if (defined?(Rails.env) ? Rails.env.production? : "production" == RAILS_ENV)
+        if (ENV['HEROKU_SLUG'])
           if defined?(Rails.configuration) && defined?(Rails.configuration.middleware)
             Rails.configuration.middleware.use(Hassle)
           elsif defined?(ActionController::Dispatcher) && defined?(ActionController::Dispatcher.middleware)
